c101d7118117ba8c2d0fb56ea56866ffe48d6c5b
Adarsh Mishra <40768847+iamadarsh1994@users.noreply.github.com>
2024-08-05 11:09:35 +0530

    Update db.js

--- 5/5.2/backend/db.js
+++ 5/5.2/backend/db.js
@@ -3,9 +3,8 @@ const mongoose = require ("mongoose")
 //Todo{title:string; description:string; completed:boolean}
 
 //mongodb url 
-//mongodb+srv://iamadarsh1994:tSSdKLIbXUo0aayB@cluster0.2noln53.mongodb.net/
 
-mongoose.connect("mongodb+srv://iamadarsh1994:tSSdKLIbXUo0aayB@cluster0.2noln53.mongodb.net/")
+mongoose.connect("-----...")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
